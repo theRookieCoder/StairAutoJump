b818a7a9110e5b98421468df075cf8546ba04c73
Ilesh Thiada <ileshkt@gmail.com>
2022-04-09 15:28:11 +0530

    Initial Commit

---
+++ src/main/java/io/github/therookiecoder/stairautojump/StairAutoJump.java
@@ -0,0 +1,8 @@
+package io.github.therookiecoder.stairautojump;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StairAutoJump implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
---
+++ src/main/java/io/github/therookiecoder/stairautojump/mixin/PlayerTickMixin.java
@@ -0,0 +1,31 @@
+package io.github.therookiecoder.stairautojump.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public class PlayerTickMixin {
+    private static boolean isStair(BlockPos pos, World world) {
+        BlockState block = world.getBlockState(pos);
+        return block != null && block.getBlock() instanceof StairsBlock;
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    public void injectMethod(CallbackInfo ci) {
+        PlayerEntity player = (PlayerEntity) (Object) this;
+        World world = player.getWorld();
+        BlockPos pos = player.getBlockPos();
+        GameOptions options = MinecraftClient.getInstance().options;
+
+        options.autoJump = isStair(pos, world) || isStair(pos.down(), world);
+    }
+}
